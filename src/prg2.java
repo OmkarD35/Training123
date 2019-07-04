275d36c440f08fae0d065adb09c254400e9c87a1
om0305 <52519887+om0305@users.noreply.github.com>
2019-07-04 15:23:52 +0530

    Update prg2.java

--- src/prg2.java
+++ src/prg2.java
@@ -3,6 +3,7 @@ public class prg2 {
 	public static void main(String[] args)
 	{
 	System.out.println("This is omkar hahaha in second program sending from 1.1");
+		System.out.println("This is omkar hahaha in second program sending from 1.2");
 	
 
 }
